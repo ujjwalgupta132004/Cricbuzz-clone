82671e241777dbfe003bd32094e66deb976fccbd
ujjwalgupta132004 <ujjwalgupta1309@gmail.com>
2026-04-12 11:27:47 +0530

    chore: update app shell, auth flow, and frontend package setup

--- server/models/User.js
+++ server/models/User.js
@@ -1,23 +1,30 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
+
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true, trim: true },
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true, minlength: 6 },
-    favorites: [{ type: String }],
+    favorites: [{
+        sport: { type: String, enum: ['cricket', 'football', 'tennis'] },
+        matchId: String,
+        matchName: String
+    }],
+    preferredSports: [{ type: String, enum: ['cricket', 'football', 'tennis'] }],
+    darkMode: { type: Boolean, default: false }
 }, { timestamps: true });
 
 
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+    this.password = await bcrypt.hash(this.password, await bcrypt.genSalt(10));
     next();
 });
 
-userSchema.methods.comparePassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
+userSchema.methods.comparePassword = async function (entered) {
+    return await bcrypt.compare(entered, this.password);
 };
+
 
 module.exports = mongoose.model('User', userSchema);
